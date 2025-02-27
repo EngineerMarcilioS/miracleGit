867e2c31e6502bf944873cb6977d2f3d19522de6
MarcilioS <marciliosantoslima@yahoo.com.br>
2025-02-27 16:00:02 -0300

    add line 3

--- miracleGit.py
+++ miracleGit.py
@@ -2,6 +2,7 @@ def marcilio():
     print("This is a miracle of git")
     print("This is a new line")
     print("This is a new line 2")
+    print("This is a new line 3")
 
 def sarah():
     print("This is a miracle of git sarah")
