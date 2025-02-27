394fbe722e55ecc7e5dbd48d76e4954f5af0111e
root <root@MarcilioR>
2025-02-27 15:56:45 -0300

    add line 3

--- miracleGit.py
+++ miracleGit.py
@@ -1,6 +1,7 @@
 def marcilio():
     print("This is a miracle of git")
     print("This is a new line")
+    print("This is a new line 2")
 
 def sarah():
     print("This is a miracle of git sarah")
